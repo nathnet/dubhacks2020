2e8274d9ffd994f3a14ace6df4baf1c7e6e65942
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2021-01-05 02:58:43 -0800

    Updates

---
+++ precache-manifest.3e9d0c1df5cf41f25757a482d921265e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022dc75dc3bddba3a075006f25d006a4",
+    "url": "/dubhacks2020/index.html"
+  },
+  {
+    "revision": "1eed1f6517727d4148fe",
+    "url": "/dubhacks2020/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8e137e4bcf1c09df73a4",
+    "url": "/dubhacks2020/static/css/main.34f8a87f.chunk.css"
+  },
+  {
+    "revision": "1eed1f6517727d4148fe",
+    "url": "/dubhacks2020/static/js/2.301e7904.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/dubhacks2020/static/js/2.301e7904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e137e4bcf1c09df73a4",
+    "url": "/dubhacks2020/static/js/main.20c77e8e.chunk.js"
+  },
+  {
+    "revision": "cd0767e255a036876ccd",
+    "url": "/dubhacks2020/static/js/runtime-main.12a458a3.js"
+  },
+  {
+    "revision": "21c77383824c2a52646ec450c2c2d0ff",
+    "url": "/dubhacks2020/static/media/AP.21c77383.jpg"
+  },
+  {
+    "revision": "55b60a3f05bb99171aafe39cb3124abe",
+    "url": "/dubhacks2020/static/media/ML.55b60a3f.jpg"
+  },
+  {
+    "revision": "000fb063fa8fe5dfeb8944a1406b55d6",
+    "url": "/dubhacks2020/static/media/NN.000fb063.jpg"
+  },
+  {
+    "revision": "fbcbf4437d9a67648a3ac1d385f3acbe",
+    "url": "/dubhacks2020/static/media/OpenDyslexic-Bold.fbcbf443.otf"
+  },
+  {
+    "revision": "be1e6672908f2d76091de9ac60983c89",
+    "url": "/dubhacks2020/static/media/OpenDyslexic-Regular.be1e6672.otf"
+  },
+  {
+    "revision": "b3bad4ad8f58d6bf74e842ad1da0f118",
+    "url": "/dubhacks2020/static/media/XC.b3bad4ad.jpg"
+  },
+  {
+    "revision": "3bd7b99f5e4cc75a22b2d5375ff6daa1",
+    "url": "/dubhacks2020/static/media/YW.3bd7b99f.jpg"
+  },
+  {
+    "revision": "21652d9aec8973efa8059ee29b237c2f",
+    "url": "/dubhacks2020/static/media/apple.21652d9a.gif"
+  },
+  {
+    "revision": "c667b6c6159e3a5c7aed43cf9b25db3d",
+    "url": "/dubhacks2020/static/media/banana.c667b6c6.gif"
+  },
+  {
+    "revision": "1af18f105ccc60f66ac46f56372e9f97",
+    "url": "/dubhacks2020/static/media/final.1af18f10.gif"
+  },
+  {
+    "revision": "9bb92559d41ba27417aa72a96428c132",
+    "url": "/dubhacks2020/static/media/hungry.9bb92559.gif"
+  },
+  {
+    "revision": "20bf7474277ded87a42326158cf8bae8",
+    "url": "/dubhacks2020/static/media/intro.20bf7474.gif"
+  },
+  {
+    "revision": "9f6aa87d78e242c2f6e763a873ef5b05",
+    "url": "/dubhacks2020/static/media/lexi_and_ball.9f6aa87d.png"
+  },
+  {
+    "revision": "15dabf18eb500b22785098034ee83020",
+    "url": "/dubhacks2020/static/media/lexi_eat.15dabf18.png"
+  },
+  {
+    "revision": "6cc6f5e421ed2acd6f1c071fbbc6408e",
+    "url": "/dubhacks2020/static/media/lexi_logo.6cc6f5e4.png"
+  },
+  {
+    "revision": "875307537e633239d62ac01e18616199",
+    "url": "/dubhacks2020/static/media/lexi_with_family.87530753.png"
+  },
+  {
+    "revision": "ce74de5f6172c827cf701b989dd57231",
+    "url": "/dubhacks2020/static/media/orange.ce74de5f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d78c0eca8273ee2f10003609afa3744.js"
+  "/dubhacks2020/precache-manifest.3e9d0c1df5cf41f25757a482d921265e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dubhacks2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
